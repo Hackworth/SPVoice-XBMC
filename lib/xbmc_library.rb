ee41e3cad86ec8101913de5e7603bbd8ddbde6a9
Jordan Hackworth <git@jordanhackworth.com>
2012-09-23 21:44:36 -0700

    Intigrating TV show and movie matching

--- lib/xbmc_library.rb
+++ lib/xbmc_library.rb
@@ -116,7 +116,7 @@ class XBMCLibrary
       tvshows = xbmc('VideoLibrary.GetTVShows')["tvshows"]
       movies = xbmc('VideoLibrary.GetMovies', { :properties => ["file", "genre", "director", "title", "originaltitle", "runtime", "year", "playcount", "rating", "lastplayed"] })["movies"]
     end
-    media = tvshows
+    media = tvshows.zip(movies).collect { |array| array.inject(&:merge) }
     matcher = FuzzyMatch.new(media.each { |video| video["label"] })
     result = matcher.find(title)
     return result
