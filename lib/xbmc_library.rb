ef8d996ab67136e8c1af9a10d7ab1366f32d3bea
Jordan Hackworth <git@jordanhackworth.com>
2012-09-26 19:25:25 -0700

    Testing

--- lib/xbmc_library.rb
+++ lib/xbmc_library.rb
@@ -251,7 +251,9 @@ class XBMCLibrary
   end
 
   def recent_episodes
-    episodes = xbmc('VideoLibrary.GetRecentlyAddedEpisodes', { :limits => 10, :properties => ["title"] } )["episodes"]
+    episodes = "test"
+    puts "test3"
+    #episodes = xbmc('VideoLibrary.GetRecentlyAddedEpisodes', { :limits => 10, :properties => ["title"] } )["episodes"]
     return episodes
   end
 
